6e25571e9f8454536d780ee10c455787d7c9b0a3
Wenji Shen <swjoanna795@gmail.com>
2025-09-30 15:56:52 +1300

    fix: demo

--- demo.py
+++ demo.py
@@ -2,13 +2,13 @@ import requests
 
 login_data = {
     "method": "post",
-    "url": "http://127.0.0.1:8888/api/private/v1/login",
+    "url": "http://8.138.193.96:8888/api/private/v1/login",
     "data": {"username": "admin", "password": "123456"}
 }
 
 upload_data = {
     "method": "post",
-    "url": "http://127.0.0.1:8888/api/private/v1/upload",
+    "url": "http://8.138.193.96:8888/api/private/v1/upload",
     "headers": None,
     "files": None
 }
@@ -16,7 +16,7 @@ upload_data = {
 # === 获取 token ===
 res1 = requests.request(**login_data)
 token = res1.json()["data"]["token"]
-print(token)
+print(":)打印token: ", token)
 
 # === 文件上传 ===
 upload_data["headers"] = {"Authorization": token}
@@ -26,7 +26,7 @@ upload_data["headers"] = {"Authorization": token}
 # 元组的结构为 (文件名, 文件对象, 文件类型)
 # 例如: ("file", ("1.jpg", open("./file/1.jpg", "rb"), "jpg"))
 
-upload_data["files"] = {"file": ("1.jpg", open("./file/1.jpg", "rb"), "jpg")}
+upload_data["files"] = {"file": ("IMG_2867.jpg", open("./file/IMG_2867.jpg", "rb"), "jpg")}
 
 res2 = requests.request(**upload_data)
-print(res2.json())
+print(":)res2json: ", res2.json())
